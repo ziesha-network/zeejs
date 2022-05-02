7aee77f535146fa5fa8cfa8ec9807692363af886
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2022-05-02 19:29:54 +0430

    Define Field class

--- zeejs.js
+++ zeejs.js
@@ -1,3 +1,25 @@
 const MODULUS = BigInt("52435875175126190479447740508185965837690552500527637822603658699938581184513");
 
-alert((MODULUS + 1n) % MODULUS);
+function Field(val) {
+  this.value = ((BigInt(val) % MODULUS) + MODULUS) % MODULUS;
+}
+
+Field.prototype.neg = function() {
+  return new Field(MODULUS - this.value);
+}
+
+Field.prototype.add = function(other) {
+  return new Field(this.value + other.value);
+}
+
+Field.prototype.sub = function(other) {
+  return new Field(this.value - other.value);
+}
+
+Field.prototype.mul = function(other) {
+  return new Field(this.value * other.value);
+}
+
+Field.prototype.toString = function() {
+  return this.value.toString();
+}
