d06a453419f6bcd5692ee994bea67d9b610dd93e
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-02-15 14:56:38 +0330

    Swtich to pelmeni-6

--- js/wallet.js
+++ js/wallet.js
@@ -129,7 +129,7 @@ PrivateKey.prototype.create_tx = function (nonce, to, amount, fee) {
 
 var STATE = { sk: null, account: null };
 let NODE = "213.14.138.127:8765";
-let NETWORK = "pelmeni-5";
+let NETWORK = "pelmeni-6";
 let POOLS = [
   "213.14.138.127:8765",
   "117.62.66.67:8765",
