1ca275940404043d042d32b227130077cac6ae35
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-05-03 11:29:12 +0430

    Switch to peking-duck net

--- js/wallet.js
+++ js/wallet.js
@@ -136,7 +136,7 @@ PrivateKey.prototype.create_tx = function (
 
 var STATE = { sk: null, account: null };
 let NODE = "api.ziesha.network:9766";
-let NETWORK = "koobideh-kebab";
+let NETWORK = "peking-duck";
 let POOLS = [
   NODE
 ];
