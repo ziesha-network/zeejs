172eaf3b2046398fd19ec2dc7bbb34349376b969
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-05-29 13:15:29 +0430

    Switch to deruny-3

--- js/wallet.js
+++ js/wallet.js
@@ -136,7 +136,7 @@ PrivateKey.prototype.create_tx = function (
 
 var STATE = { sk: null, account: null };
 let NODE = "api.ziesha.network:9766";
-let NETWORK = "burrito";
+let NETWORK = "deruny-3";
 let POOLS = [
   NODE
 ];
